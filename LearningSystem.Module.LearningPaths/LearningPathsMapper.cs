a4ec61d76d6fb78da3a04b7baea6b8323340f5d7
Ionuț Dănilă <hello@ionutdanila.com>
2021-06-14 20:06:54 +0300

    Test boilerplate generator

--- LearningSystem.Module.LearningPaths/LearningPathsMapper.cs
+++ LearningSystem.Module.LearningPaths/LearningPathsMapper.cs
@@ -1,7 +1,5 @@
 ﻿using AutoMapper;
 using LearningSystem.Module.Data.Models;
-using LearningSystem.Module.LearningPaths.Application.Commands.LearningPaths.Create.Models;
-using LearningSystem.Module.LearningPaths.Application.Commands.LearningPaths.Update.Models;
 using LearningSystem.Module.LearningPaths.Domain.Models;
 
 namespace LearningSystem.Module.LearningPaths
@@ -10,13 +8,7 @@ namespace LearningSystem.Module.LearningPaths
     {
         public LearningPathsMapper()
         {
-            CreateMap<LearningPath, LearningPathDomainModel>();
-
             CreateMap<Tutorial, TutorialDomainModel>();
-
-            CreateMap<CreateLearningPathRequestModel, LearningPath>();
-
-            CreateMap<UpdateLearningPathRequestModel, LearningPath>();
         }
     }
 }
